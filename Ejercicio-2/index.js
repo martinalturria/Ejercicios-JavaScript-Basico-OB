6be73de0da2eeba59718e4ec6032acea6ea8b863
Martin Alturria <martinalturria@hotmail.com>
2023-03-01 10:33:08 -0300

    Agrego Ejercicio 2 Finalizado

---
+++ Ejercicio-2/index.js
@@ -0,0 +1,8 @@
+const lista = ['Martin Alturria', 35, true, new Date(1987, 11, 15), {
+    titulo: 'Aprendiendo JavaScript',
+    autor: 'Carlos Azaustre',
+    fecha: 2023,
+    url: 'https://leanpub.com/aprendiendo-javascript'
+}]
+
+console.log(lista);
