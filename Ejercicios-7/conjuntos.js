ff708694c9b10d5960bff5cf2159f94ccd1a891e
Martin Alturria <martinalturria@hotmail.com>
2023-03-03 19:24:44 -0300

    Agrego Ejercicio 7 - Set

---
+++ Ejercicios-7/conjuntos.js
@@ -0,0 +1,7 @@
+const familia = ['Maria','Martin', 'Juani', 'Gabi', 'Bauti']
+const setFamilia = new Set(familia);
+
+setFamilia.add('Martin')
+setFamilia.add('JavaScript')
+
+console.log(setFamilia);
