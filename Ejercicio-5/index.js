85a6611f6c259f19454740c1720d9d66998a8638
Martin Alturria <martinalturria@hotmail.com>
2023-03-03 09:55:24 -0300

    Agrego Ejercicio 5 - Numeros Finalizado

---
+++ Ejercicio-5/index.js
@@ -0,0 +1,6 @@
+let alturaCms = 175;
+let alturaMts = 1.75;
+let peso = 65.4;
+let alturaRedo = Math.ceil(alturaMts);
+let pesoRedo = Math.floor(peso);
+let igualdad = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
