7e60939d4b86bb2604b75575e6c2671e5f7cccf9
Martin Alturria <martinalturria@hotmail.com>
2023-03-06 14:10:39 -0300

    Agrego Ejercicio-10-Modulos Finalizado

---
+++ Ejercicio-10/index.js
@@ -0,0 +1,6 @@
+import {suma, multiplica} from './controller.js';
+import chalk from 'chalk';
+
+const multiplicaion = multiplica(suma(1, 2), suma(4, 5));
+
+console.log(chalk.green(multiplicaion));
