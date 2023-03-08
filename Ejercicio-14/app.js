94bd5cffdadd3c7cd87e35ae2560ef33664f7ae3
Martin Alturria <martinalturria@hotmail.com>
2023-03-08 12:11:56 -0300

    Agrego Ejercicio-14-HTML y JS Finalizado

---
+++ Ejercicio-14/app.js
@@ -0,0 +1,9 @@
+const boton = document.querySelector("#btn");
+
+boton.addEventListener("click", e => {
+    alert("Click en el Botón");
+})
+
+$("boton").click(function() {
+    console.log("Hola, estoy utilizando jQuery")
+})
